26003a86ba68a123bb22f740b84d22a242cd668a
Ruben Ernst <ruben.ernst@bluewin.ch>
2024-04-21 18:22:45 +0200

    Multifix

--- code/3_plots.R
+++ code/3_plots.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
 library(tidyverse)
+library(ggrepel)
 
 
 ### Descriptive stats ----
@@ -9,24 +10,25 @@ load("data/zillow_county.RData")
 # ZHVI median by data series
 zhvi_by_data_series_line <- aggregate(zhvi ~ data_series + lubridate::year(date), zillow_county, median, na.rm = T) |>
   select(data_series, "year" = `lubridate::year(date)`, zhvi) |>
-  mutate(data_series = factor(data_series, levels = c("all_homes_bottom_tier", "all_homes_top_tier", "single_family_homes", "condo_coop", "one_bedroom", "two_bedroom", "three_bedroom", "four_bedroom", "five_plus_bedroom"))) |> 
+  mutate(data_series = factor(data_series, levels = c("all_homes_bottom_tier", "all_homes_middle_tier", "all_homes_top_tier", "single_family_homes", "condo_coop", "one_bedroom", "two_bedroom", "three_bedroom", "four_bedroom", "five_plus_bedroom"))) |> 
   ggplot(aes(x = year, y = zhvi, linetype = data_series)) +
   geom_line() +
-  geom_text(data = aggregate(zhvi ~ data_series + lubridate::year(date), zillow_county, median, na.rm = T) |> select(data_series, "year" = `lubridate::year(date)`, zhvi) |> filter(year == last(year)),
-            aes(label = plyr::revalue(data_series, c("all_homes_bottom_tier" = "Bottom Tier",
-                                                     "all_homes_top_tier" = "Top Tier",
-                                                     "single_family_homes" = "Single Family",
-                                                     "condo_coop" = "Condo / Co-Op",
-                                                     "one_bedroom" = "1 Bedroom",
-                                                     "two_bedroom" = "2 Bedrooms",
-                                                     "three_bedroom" = "3 Bedrooms",
-                                                     "four_bedroom" = "4 Bedrooms",
-                                                     "five_plus_bedroom" = "5+ Bedrooms")),
-                x = year, y = zhvi),
-            hjust = 0.6, vjust = -0.5) +
+  geom_text_repel(data = aggregate(zhvi ~ data_series + lubridate::year(date), zillow_county, median, na.rm = T) |> select(data_series, "year" = `lubridate::year(date)`, zhvi) |> filter(year == last(year)),
+                  aes(label = plyr::revalue(data_series, c("all_homes_bottom_tier" = "Bottom Tier",
+                                                           "all_homes_middle_tier" = "Middle Tier",
+                                                           "all_homes_top_tier" = "Top Tier",
+                                                           "single_family_homes" = "Single Family",
+                                                           "condo_coop" = "Condo / Co-Op",
+                                                           "one_bedroom" = "1 Bedroom",
+                                                           "two_bedroom" = "2 Bedrooms",
+                                                           "three_bedroom" = "3 Bedrooms",
+                                                           "four_bedroom" = "4 Bedrooms",
+                                                           "five_plus_bedroom" = "5+ Bedrooms")),
+                      x = year, y = zhvi),
+                  hjust = 0.6, vjust = -1.5, size = 3) +
   scale_x_continuous(limits = c(2000, 2024), breaks = seq(2000, 2024, 2)) +
-  scale_y_continuous(labels = function(x){format(x, big.mark = "'", scientific = FALSE)}, name = "Zillow Home Value Index") +
-  scale_linetype(name = "Data Series", labels = c("Bottom Tier", "Top Tier", "Single Family", "Condo / Co-Op", "1 Bedroom", "2 Bedrooms", "3 Bedrooms", "4 Bedrooms", "5+ Bedrooms")) +
+  scale_y_continuous(labels = function(x){format(x, big.mark = "'", scientific = FALSE)}, name = "Zillow Home Value Index", limits = c(0, 450000)) +
+  scale_linetype(name = "Data Series", labels = c("Bottom Tier", "Middle Tier", "Top Tier", "Single Family", "Condo / Co-Op", "1 Bedroom", "2 Bedrooms", "3 Bedrooms", "4 Bedrooms", "5+ Bedrooms")) +
   theme_bw() +
   theme(legend.position = "bottom",
         axis.title.x = element_blank(),
@@ -34,4 +36,8 @@ zhvi_by_data_series_line <- aggregate(zhvi ~ data_series + lubridate::year(date)
         plot.margin = margin(1, 1, 1, 1, "points"),
         panel.spacing = margin(1, 80, 1, 1, "points"))
 
-ggsave(filename = "plots/zhvi_by_data_series_line.pdf", plot = zhvi_by_data_series_line, width = 12, height = 9)
+ggsave(filename = "plots/zhvi_by_data_series_line.pdf", plot = zhvi_by_data_series_line, width = 10, height = 7.5)
+
+
+
+# TODO FEMA disaster maps by incident_category
