497b8a88b531a3f4fd955e8bfc57a93e10edc8af
Ruben Ernst <ruben.ernst@bluewin.ch>
2024-06-14 14:27:09 +0200

    Add descriptive statistics for economic variables

--- code/2_descriptive stats.R
+++ code/2_descriptive stats.R
@@ -114,3 +114,76 @@ fema_desc |>
   kable_classic(full_width = TRUE) |> 
   write_file(file = "tables/fema_descriptives.tex")
 
+
+### Descriptives on BEA / LAUS ----
+load("data/bea_gdp.RData")
+load("data/bls_laus.RData")
+load("data/bls_qcew.RData")
+
+laus$fips_code <- fips_pad(laus$state_code, laus$county_code)
+
+econ_desc <- merge(merge(subset(select(qcew, "fips_code" = area_fips, agglvl_code, year, qtr, avg_wkly_wage), agglvl_code == 70 & avg_wkly_wage != 0, select = c(fips_code, year, qtr, avg_wkly_wage)),
+                         select(laus, fips_code, year, unemployment_rate), by = c("fips_code", "year"), all.x = TRUE),
+                   subset(bea, table_name == "CAGDP2" & industry_classification == "...", select = c(fips_code, gdp_value, year)), by = c("fips_code", "year"), all.x = TRUE) |> 
+  subset(year >= "2000") |> 
+  mutate("unemployment_rate" = as.numeric(unemployment_rate), # NAs introduced through 368 "N.A." strings, checked and okay
+         "gdp_value" = as.numeric(gdp_value)) |> 
+  pivot_longer(cols = c("avg_wkly_wage", "unemployment_rate", "gdp_value"), names_to = "data_series", values_to = "econ_value") |> 
+  mutate("date" = paste0(year, "-", str_pad(qtr, width = 2, side = "left", pad = "0")))
+
+econ_desc_out <- merge(select(aggregate(econ_value ~ data_series, econ_desc, function(x){length(na.omit(x))}), data_series, "observations" = econ_value),
+                       select(aggregate(date ~ data_series, subset(econ_desc, !is.na(econ_value)), min), data_series, "min_date" = date),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(date ~ data_series, subset(econ_desc, !is.na(econ_value)), max), data_series, "max_date" = date),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out$date_range <- paste0(econ_desc_out$min_date, " - ", econ_desc_out$max_date)
+econ_desc_out$min_date <- NULL
+econ_desc_out$max_date <- NULL
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(fips_code ~ data_series, subset(econ_desc, !is.na(econ_value)), function(x){length(unique(x))}), data_series, "nr_regions" = fips_code),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       as.data.frame(as.matrix(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), function(x){quantile(x, c(0, 0.25, 0.5, 0.75, 1), na.rm = T)}))),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), mean), data_series, "mean_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), function(x_){density(x_, na.rm = T)$x[which.max(density(x_, na.rm = T)$y)]}), data_series, "mode_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), var), data_series, "var_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), sd), data_series, "sd_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), moments::skewness), data_series, "skewness_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- merge(econ_desc_out,
+                       select(aggregate(econ_value ~ data_series, subset(econ_desc, !is.na(econ_value)), moments::kurtosis), data_series, "kurtosis_econ_value" = econ_value),
+                       by = "data_series", all.x = TRUE)
+
+econ_desc_out <- as.data.frame(t(econ_desc_out))
+names(econ_desc_out) <- econ_desc_out[1, ]
+econ_desc_out <- econ_desc_out[-1,]
+econ_desc_out <- select(econ_desc_out, unemployment_rate, avg_wkly_wage, gdp_value)
+names(econ_desc_out) <- c("Unemployment rate", "Average weekly wage", "GDP")
+row.names(econ_desc_out) <- c("Observations", "Date Range", "Regions", "Minimum", "25% Quantile", "Median", "75% Quantile", "Maximum", "Mean", "Mode", "Variance", "Standard Deviation", "Skewness", "Kurtosis")
+
+# Save as Latex table
+econ_desc_out |> 
+  kbl(format = "latex", align = "r") |> 
+  kable_classic(full_width = TRUE) |> 
+  write_file(file = "tables/econ_descriptives.tex")
